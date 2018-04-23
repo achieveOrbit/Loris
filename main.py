4c9b01bdea492b3b59ede6bc03d645434014a63f
Lukáš Hejcman <lukas.hejcman@outlook.com>
2018-04-23 12:33:22 +0200

    Added month and year progress

--- main.py
+++ main.py
@@ -13,6 +13,28 @@ import sys
 #TODO: Add progress for month and year
 #TODO: Fullscreen dialog with the passing time (possibly a message window or something)
 
+
+days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+
+def year_progress(time_now):
+    current_month = time_now.month
+    time_days = time_now.day
+    for x in range(0, current_month-1):
+        time_days += days_in_month[x]
+    if time_now.year % 4 == 0:
+        percentage_passed_year = "{:.2%}".format(time_days / 366)
+    else:
+        percentage_passed_year = "{:.2%}".format(time_days / 365)
+    return percentage_passed_year
+
+def month_progress(time_now):
+    current_month = time_now.month
+    current_day = time_now.day
+    percentage_passed_month = "{:.2%}".format(current_day/days_in_month[current_month])
+    return percentage_passed_month
+
+
 def date_progress(time_now):
     time_seconds = float((int(time_now.hour)*3600) + (int(time_now.minute)*60) + int(time_now.second))
     percentage_passed_day = "{:.2%}".format(time_seconds/86400.0)
@@ -43,11 +65,13 @@ if __name__ == "__main__":
     systray.start()
 
     while True:
-        time_now = datetime.datetime.now().time()
+        time_now = datetime.datetime.now()
+        percentage_passed_year = "Year's progress: " + str(year_progress(time_now))
+        percentage_passed_month = "Month's progress: " + str(month_progress(time_now))
         percentage_passed_day = "Today's progress: " + str(date_progress(time_now))
         percentage_passed_hour = "Hour's progress: " + str(hour_progress(time_now))
         percentage_passed_minute = "Minute's progress: " + str(minute_progress(time_now))
-        hover_text = percentage_passed_day + "\n" + percentage_passed_hour + "\n" + percentage_passed_minute
+        hover_text = percentage_passed_year + "\n" + percentage_passed_month + "\n" + percentage_passed_day + "\n" + percentage_passed_hour + "\n" + percentage_passed_minute
         systray.update(hover_text = hover_text)
         time.sleep(1)
 
